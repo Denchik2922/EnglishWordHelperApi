20f43f249f0626386e563ad696d23d1201aacf2f
Yavor Denys <denys.yavor@intapp.com>
2022-04-10 17:01:14 +0300

    Added unit tests for BLL

---
+++ BLL.Tests/Fixtures/WordServiceFixture.cs
@@ -0,0 +1,15 @@
+﻿using BLL.Services;
+using BLL.Tests.Helpers;
+
+namespace BLL.Tests.Fixtures
+{
+    public class WordServiceFixture
+    {
+        public WordService Create()
+        {
+            var context = new DBContextHelper().Context;
+            var mapper = MapperHelper.GetInstance();
+            return new WordService(context, mapper);
+        }
+    }
+}
